f4c0475fa5c6e17def9e8aac2762d177b80d8178
Mike D <mikedaug@gmail.com>
2018-07-28 10:16:15 -0400

    completed work through end of page 52. All tests pass Need to add 3rd problem view.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -14,24 +14,36 @@ describe('LearnJS', function() {
     learnjs.showView('#problem-42');
     expect(learnjs.problemView).toHaveBeenCalledWith('42');
   });
-  
+
   it('invokes the router when loaded', function() {
     spyOn(learnjs, 'showView');
     learnjs.appOnReady();
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
   });
-  
+
   it('subscribes to the hash change event', function() {
-  	learnjs.appOnReady();
-  	spyOn(learnjs, 'showView');
-  	$(window).trigger('hashchange');
-  	expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
   });
 
   describe('problem view', function() {
+    var view;
+    beforeEach(function() {
+      view = learnjs.problemView('1');
+    });
+
     it('has a title that includes the problem number', function() {
-      var view = learnjs.problemView('1');
       expect(view.find('.title').text()).toEqual('Problem #1');
+    });
+
+    it('shows the description', function() {
+      expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+    });
+
+    it('shows the problem code', function() {
+      expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
     });
   });
 });
